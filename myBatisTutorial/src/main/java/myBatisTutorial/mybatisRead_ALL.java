6c884e440a78c2fd4b4ea41653b7a16e89f9f37e
francy328 <fviscomi@gmail.com>
2018-04-08 12:06:53 +0200

    first commmit: file minimi necessari per configurare MyBatis

---
+++ myBatisTutorial/src/main/java/myBatisTutorial/mybatisRead_ALL.java
@@ -0,0 +1,33 @@
+package myBatisTutorial;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.util.List;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class mybatisRead_ALL {
+	 public static void main(String args[]) throws IOException{
+
+	      Reader reader = Resources.getResourceAsReader("SqlMapConfig.xml");
+	      SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);		
+	      SqlSession session = sqlSessionFactory.openSession();
+	      
+	      //select contact all contacts		
+	      List<Studente> student = session.selectList("Studente.getAll");
+	          
+	      for(Studente st : student ){    	  
+	         System.out.println(st.getId());
+	         System.out.println(st.getNome());
+	         System.out.println(st.getCognome());   
+	      }  
+			
+	      System.out.println("Records Read Successfully ");          
+	      session.commit();   
+	      session.close();			
+	   }
+
+}
