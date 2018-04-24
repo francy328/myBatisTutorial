a19c06813b57cb853d7da13d087ffbd1a131b8f7
francy328 <fviscomi@gmail.com>
2018-04-25 00:07:29 +0200

    veramente

--- myBatisTutorial/src/main/java/myBatisTutorial/mybatisRead_ALL.java
+++ myBatisTutorial/src/main/java/myBatisTutorial/mybatisRead_ALL.java
@@ -26,7 +26,7 @@ public class mybatisRead_ALL {
 	      }  
 			
 	      System.out.println("Records Read Successfully ");  
-	      //im going to loose patience veramente
+	      //im going to loose patience veramente veramente
 	      session.commit();   
 	      session.close();			
 	   }
